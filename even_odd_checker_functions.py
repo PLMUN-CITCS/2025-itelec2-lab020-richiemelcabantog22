245ce2c2f31a91d790d69c3a92b0b43fbaf12095
richiemelcabantog22 <>
2025-03-11 23:38:45 +0800

    Last Sync: 2025-03-11 15:38:45 (Mobile)

--- even_odd_checker_functions.py
+++ even_odd_checker_functions.py
@@ -1,16 +1,18 @@
 def get_integer_input():
-   number = int(input("Enter an Integer: "))
-   return number
-
+    number = int(input("Enter an Integer: "))
+    return number
 
 def check_even_odd(number):
-   if number % 2 == 0:
-       return "Even"
-   else:
-       return "Odd"
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
+def main():
+    number = get_integer_input()
+    n1 = number
+    number = check_even_odd(number)
+    print(f"{n1} is an {number} number")
 
-number = get_integer_input()
-n1 = number
-number = check_even_odd(number)
-print(f"{n1} is an {number} number")
+if __name__ == "__main__":
+    main()
